9086f9f337aaa00b08db073ca4c9b2a5cd17a2ff
programmerlab <kroy.iips@gmail.com>
2018-08-18 16:35:47 +0530

    changes

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "kroy" );
+define( "RECIPIENT_EMAIL", "kroy.iips@mail.com" );
 
 
 // Read the form values
